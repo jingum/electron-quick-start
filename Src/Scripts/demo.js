974aec2b7315b8fa832cf064d112cc1fd1b24072
jingum <jingum@qq.com>
2017-09-27 22:50:45 +0800

    1. 添加js 外部影响demo

---
+++ Src/Scripts/demo.js
@@ -0,0 +1,6 @@
+function  Fun2(){
+    console.log('2222');
+    alert("2222");
+}
+
+Fun2();
--- renderer.js
+++ renderer.js
@@ -1,3 +1,9 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+function  Fun1(){
+    console.log('1111');
+    alert("11111");
+}
+require("./Src/Scripts/demo.js")
+Fun1();
